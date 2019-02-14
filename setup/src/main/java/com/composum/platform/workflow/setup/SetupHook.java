a07e42df476ea1772a2d06c862661d34af1164ef
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-02-14 10:16:06 +0100

    SethupHooks: throw PackageException when something goes wrong

--- setup/src/main/java/com/composum/platform/workflow/setup/SetupHook.java
+++ setup/src/main/java/com/composum/platform/workflow/setup/SetupHook.java
@@ -4,6 +4,7 @@ import com.composum.sling.core.service.RepositorySetupService;
 import com.composum.sling.core.setup.util.SetupUtil;
 import org.apache.jackrabbit.vault.packaging.InstallContext;
 import org.apache.jackrabbit.vault.packaging.InstallHook;
+import org.apache.jackrabbit.vault.packaging.PackageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +21,7 @@ public class SetupHook implements InstallHook {
 
     @Override
     @SuppressWarnings("SwitchStatementWithTooFewBranches")
-    public void execute(InstallContext ctx) {
+    public void execute(InstallContext ctx) throws PackageException {
         switch (ctx.getPhase()) {
             case INSTALLED:
                 LOG.info("installed: execute...");
@@ -30,14 +31,15 @@ public class SetupHook implements InstallHook {
         }
     }
 
-    protected void setupAcls(InstallContext ctx) {
+    protected void setupAcls(InstallContext ctx) throws PackageException {
         RepositorySetupService setupService = SetupUtil.getService(RepositorySetupService.class);
         try {
             Session session = ctx.getSession();
             setupService.addJsonAcl(session, SETUP_ACLS, null);
             session.save();
-        } catch (RepositoryException | IOException rex) {
+        } catch (RepositoryException | IOException | RuntimeException rex) {
             LOG.error(rex.getMessage(), rex);
+            throw new PackageException(rex);
         }
     }
 }
